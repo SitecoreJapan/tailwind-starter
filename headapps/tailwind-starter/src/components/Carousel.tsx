b4ea254af3995f2b0719d2a7f6864b194a718254
Shinichi Haramizu <shinichi@haramizu.jp>
2025-02-10 15:35:27 +0900

    update templates

--- headapps/tailwind-starter/src/components/Carousel.tsx
+++ headapps/tailwind-starter/src/components/Carousel.tsx
@@ -1,13 +1,59 @@
 import React from 'react';
-import { ComponentParams, ComponentRendering } from '@sitecore-jss/sitecore-jss-nextjs';
+import {
+  TextField,
+  useSitecoreContext,
+  SitecoreContextValue,
+} from '@sitecore-jss/sitecore-jss-nextjs';
 
-interface CarouselProps {
-  rendering: ComponentRendering & { params: ComponentParams };
-  params: ComponentParams;
+type ResultsSlideLink = {
+  fields: {
+    id: string;
+    jsonValue: JSON;
+    name: string;
+  };
+};
+
+interface Fields {
+  data: {
+    datasource: {
+      children: {
+        results: {
+          fields: ResultsSlideLink[];
+        };
+      };
+      field: {
+        title: TextField;
+      };
+    };
+  };
 }
 
+interface CarouselProps {
+  params: { [key: string]: string };
+  fields: Fields;
+}
+
+const getLocale = function (props: SitecoreContextValue): string {
+  let locale;
+
+  if (!props.language || props.language === `en`) {
+    locale = '';
+  } else {
+    locale = '/' + props.language;
+  }
+
+  return locale;
+};
+
 export const Default = (props: CarouselProps): JSX.Element => {
+  const { sitecoreContext } = useSitecoreContext();
   const id = props.params.RenderingIdentifier;
+
+  const contentLocale = getLocale(sitecoreContext);
+  const datasource = props.fields?.data?.datasource;
+
+  console.log('locale:', contentLocale);
+  console.log('datasource:', datasource);
 
   return (
     <div className={`component ${props.params.styles}`} id={id ? id : undefined}>
