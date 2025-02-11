1c99c4e8cb80367532a36af52cb0ea5478e6a5e1
Shinichi Haramizu <shinichi@haramizu.jp>
2025-02-11 13:58:19 +0900

    update Carousel component

--- headapps/tailwind-starter/src/components/Carousel.tsx
+++ headapps/tailwind-starter/src/components/Carousel.tsx
@@ -3,6 +3,7 @@ import { TextField } from '@sitecore-jss/sitecore-jss-nextjs';
 import useEmblaCarousel from 'embla-carousel-react';
 import Autoplay from 'embla-carousel-autoplay';
 import Image from 'next/image';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
 
 interface ImageValue {
   src: string;
@@ -69,11 +70,11 @@ export const Default = (props: CarouselProps): JSX.Element => {
   return (
     <div className={`component carousel ${props.params.styles}`} id={id ? id : undefined}>
       <div className="component-content">
-        <div className="relative h-1/2 mx-auto">
+        <div className="relative mx-auto">
           <div className="overflow-hidden" ref={emblaRef}>
             <div className="flex">
               {imageFields.map((field, index) => (
-                <div className="flex-[0_0_100%] h-1/2 min-w-full" key={index}>
+                <div className="flex-[0_0_100%] min-w-full" key={index}>
                   <Image
                     src={(field.jsonValue.value as ImageValue).src}
                     alt={(field.jsonValue.value as ImageValue).alt}
@@ -91,13 +92,13 @@ export const Default = (props: CarouselProps): JSX.Element => {
             className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-black/50 text-white p-2 rounded-full"
             onClick={scrollPrev}
           >
-            ◀
+            <ChevronLeft />
           </button>
           <button
             className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-black/50 text-white p-2 rounded-full"
             onClick={scrollNext}
           >
-            ▶
+            <ChevronRight />
           </button>
         </div>
       </div>
